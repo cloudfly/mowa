7bed019e05c02e4ba820ef78cd7dec3be8bf5427
chenyunfei <chenyunfei.cs@gmail.com>
2019-06-16 23:36:41 +0800

    把 dconf 和 cluster 的代码单独一个 package

---
+++ internal/addons.go
@@ -0,0 +1,133 @@
+package internal
+
+// 内置一些默认的 path，提供一些通用的功能
+
+import (
+	"encoding/json"
+
+	"github.com/cloudfly/golang/cluster"
+	"github.com/cloudfly/golang/dconf"
+	"github.com/cloudfly/mowa"
+)
+
+type ctxKey string
+
+var (
+	clt  *cluster.Cluster
+	conf dconf.DConf
+)
+
+// ClusterNodes 返回 cluster 节点列表
+func ClusterNodes(ctx *mowa.Context) interface{} {
+	return mowa.Data(clt.Nodes())
+}
+
+// ClusterAddNode 向集群增加节点
+func ClusterAddNode(ctx *mowa.Context) interface{} {
+	var node cluster.Node
+	if err := json.Unmarshal(ctx.ReadBody(), &node); err != nil {
+		return mowa.ErrorWithCode(400, err)
+	}
+	if err := clt.AddNode(node); err != nil {
+		return mowa.ErrorWithCode(400, err)
+	}
+	return mowa.Data("CREATED")
+}
+
+// ClusterUpdateNode 更新集群节点
+func ClusterUpdateNode(ctx *mowa.Context) interface{} {
+	var node cluster.Node
+	if err := json.Unmarshal(ctx.ReadBody(), &node); err != nil {
+		return mowa.ErrorWithCode(400, err)
+	}
+	if err := clt.UpdateNode(node); err != nil {
+		return mowa.ErrorWithCode(400, err)
+	}
+	return mowa.Data("UPDATED")
+}
+
+// ClusterRemoveNode 删除集群节点
+func ClusterRemoveNode(ctx *mowa.Context) interface{} {
+	name := ctx.String("name", "")
+	if name == "" {
+		return mowa.ErrorWithCode(400, "node name required")
+	}
+	if err := clt.RemoveNode(name); err != nil {
+		return mowa.ErrorWithCode(400, err)
+	}
+	return mowa.Data("DELETED")
+}
+
+// ConfigRead read a value from dconf
+func ConfigRead(ctx *mowa.Context) interface{} {
+	key := ctx.String("key", "")
+	if key == "" {
+		return mowa.ErrorWithCode(400, "key required")
+	}
+	v, err := conf.Get(key)
+	if err != nil {
+		return mowa.ErrorWithCode(404, err)
+	}
+	return mowa.Data(v)
+}
+
+// ConfigWrite write a key-value into dconf
+func ConfigWrite(ctx *mowa.Context) interface{} {
+	key := ctx.String("key", "")
+	if key == "" {
+		return mowa.ErrorWithCode(400, "key required")
+	}
+	if err := conf.Set(key, string(ctx.ReadBody()), ctx.Query("pre_exist", "false") == "true"); err != nil {
+		return mowa.ErrorWithCode(404, err)
+	}
+	return mowa.Data("OK")
+}
+
+// ConfigDelete del a key-value from dconf
+func ConfigDelete(ctx *mowa.Context) interface{} {
+	key := ctx.String("key", "")
+	if key == "" {
+		return mowa.ErrorWithCode(400, "key required")
+	}
+	if err := conf.Del(key); err != nil {
+		return mowa.ErrorWithCode(500, err)
+	}
+	return mowa.Data("OK")
+}
+
+// ConfigKeys get keys list by prefix
+func ConfigKeys(ctx *mowa.Context) interface{} {
+	return mowa.Data(conf.Keys(ctx.String("prefix", "")))
+}
+
+// ConfigData get key-values by prefix
+func ConfigData(ctx *mowa.Context) interface{} {
+	return mowa.Data(conf.Data(ctx.String("prefix", "")))
+}
+
+// WithCluster add http api for cluster management
+func WithCluster(api *mowa.Mowa, c *cluster.Cluster) *mowa.Mowa {
+	if c == nil {
+		return api
+	}
+	clt = c
+	api.Get("/debug/cluster/nodes", ClusterNodes)
+	api.Post("/debug/cluster/nodes", ClusterAddNode)
+	api.Put("/debug/cluster/nodes", ClusterUpdateNode)
+	api.Delete("/debug/cluster/nodes/:name", ClusterRemoveNode)
+	return api
+}
+
+// WithDConf add http api for dconf
+func WithDConf(api *mowa.Mowa, c dconf.DConf) *mowa.Mowa {
+	if conf == nil {
+		return api
+	}
+	conf = c
+	api.Get("/debug/config/:key", ConfigRead)
+	api.Post("/debug/config/:key", ConfigWrite)
+	api.Delete("/debug/config/:key", ConfigDelete)
+	api.Get("/debug/config_keys/:prefix", ConfigKeys)
+	api.Get("/debug/config_data/:prefix", ConfigData)
+	return api
+}
