0bd198d409dcb5b1ad7c0e3889d61bae5e7f7bbc
chenyunfei <chenyunfei.cs@gmail.com>
2019-10-14 14:51:03 +0800

    修改 handler func 类型, 增加代码可读性
    性能方面略有下降,但可忽略, 很小的

--- handler.go
+++ handler.go
@@ -23,61 +23,45 @@ func init() {
 	varHandler = expvar.Handler()
 }
 
-// handler types
-const (
-	raw = iota // raw http.Handler
-	ht0
-	ht1
-	ht2
-	ht3
-	ht4
+// Handler function types supported
+type (
+	handleFuncRaw   = func(http.ResponseWriter, *http.Request)
+	handleFuncHook  = func(*Context)
+	handleFunc      = func(*Context) interface{}
+	handleFuncBreak = func(*Context) (interface{}, bool)
+	handleFuncCode  = func(*Context) (int, interface{})
+	handleFuncFull  = func(*Context) (int, interface{}, bool)
 )
 
 // Handler is the server handler type, switch interface{}.(type) is too slow
 type Handler struct {
-	t   rune
-	raw func(http.ResponseWriter, *http.Request)
-	h0  func(*Context)
-	h1  func(*Context) interface{}
-	h2  func(*Context) (int, interface{})
-	h3  func(*Context) (int, interface{}, bool)
-	h4  func(*Context) (interface{}, bool)
+	f interface{}
 }
 
 // NewHandler create a new handler, the given argument must be a function
 func NewHandler(f interface{}) (Handler, error) {
-	switch handler := f.(type) {
-	case http.Handler:
-		return Handler{t: raw, raw: handler.ServeHTTP}, nil
-	case func(c *Context):
-		return Handler{t: ht0, h0: handler}, nil
-	case func(c *Context) interface{}:
-		return Handler{t: ht1, h1: handler}, nil
-	case func(c *Context) (int, interface{}):
-		return Handler{t: ht2, h2: handler}, nil
-	case func(c *Context) (int, interface{}, bool):
-		return Handler{t: ht3, h3: handler}, nil
-	case func(c *Context) (interface{}, bool):
-		return Handler{t: ht4, h4: handler}, nil
+	switch f.(type) {
+	case handleFuncRaw, handleFuncHook, handleFuncCode, handleFunc, handleFuncBreak, handleFuncFull:
+		return Handler{f}, nil
 	}
 	return Handler{}, errors.New("unvalid function type for handler")
 }
 
 func (handler Handler) handle(ctx *Context) bool {
 	continuous := true
-	switch handler.t {
-	case raw:
-		handler.raw(ctx.Writer, ctx.Request)
-	case ht0:
-		handler.h0(ctx)
-	case ht1:
-		ctx.Data = handler.h1(ctx)
-	case ht2:
-		ctx.Code, ctx.Data = handler.h2(ctx)
-	case ht3:
-		ctx.Code, ctx.Data, continuous = handler.h3(ctx)
-	case ht4:
-		ctx.Data, continuous = handler.h4(ctx)
+	switch f := handler.f.(type) {
+	case handleFuncRaw:
+		f(ctx.Writer, ctx.Request)
+	case handleFunc:
+		ctx.Data = f(ctx)
+	case handleFuncHook:
+		f(ctx)
+	case handleFuncBreak:
+		ctx.Data, continuous = f(ctx)
+	case handleFuncCode:
+		ctx.Code, ctx.Data = f(ctx)
+	case handleFuncFull:
+		ctx.Code, ctx.Data, continuous = f(ctx)
 	}
 	return continuous
 }
